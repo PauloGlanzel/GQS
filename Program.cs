11c26465e440b94a3eda84e37f1782b658c262b6
Paulo Henrique Glanzel <pauloglanzel@hotmail.com>
2020-12-04 16:01:46 -0300

    Subindo Projeto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Grafica.TelaInicial;
+
+namespace Grafica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new MontarTela();
+        }
+    }
+}
---
+++ TelaInicial/MontarTela.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Grafica.TelaInicial
+{
+    public class MontarTela
+    {
+        public MontarTela()
+        {
+            Console.WriteLine("Tela Inicial");
+            Console.ReadLine();
+            
+        }
+    }
+}
